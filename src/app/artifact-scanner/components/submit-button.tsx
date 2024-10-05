ccb0813ef2555799ec0832ccafc4efc0a653afa6
山﨑亮輔 <ryosuke@yamazakiryousukes-MacBook-Air.local>
2024-10-06 03:22:50 +0900

    画像から読み取った聖遺物データをrecoilで管理するようにした

--- src/app/artifact-scanner/components/submit-button.tsx
+++ src/app/artifact-scanner/components/submit-button.tsx
@@ -1,29 +1,30 @@
-import { previewState } from "@/atoms/ocr-state";
+import { artifactDataState, imageState, previewState } from "@/atoms/ocr";
 import { Button } from "@/components/ui/button";
+import { importArtifactData } from "@/lib/utils";
 import { useFormStatus } from "react-dom";
-import { useRecoilValue } from "recoil";
-import Tesseract from "tesseract.js";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 
 const SubmitButton = () => {
   const { pending } = useFormStatus();
   const preview = useRecoilValue(previewState);
+  const image = useRecoilValue(imageState);
+  const setArtifactData = useSetRecoilState(artifactDataState);
 
-  const handleImportClick = () => {
-    if (preview) {
-      Tesseract.recognize(preview, "eng", {
-        logger: (m) => console.log(m),
-      })
-        .then(({ data: { text } }) => {
-          console.log(text);
-        })
-        .catch((err) => {
-          console.error(err);
-        });
+  const handleImportImage = async () => {
+    if (image) {
+      const imagePath = URL.createObjectURL(image);
+      const data = (await importArtifactData(imagePath)) as ArtifactImportData;
+      setArtifactData(data);
+      console.log(data);
     }
   };
 
   return (
-    <Button onClick={handleImportClick} className="w-full" disabled={pending}>
+    <Button
+      onClick={handleImportImage}
+      className="w-full"
+      disabled={!preview || pending}
+    >
       {pending ? "聖遺物をインポート中..." : "聖遺物をインポート"}
     </Button>
   );
