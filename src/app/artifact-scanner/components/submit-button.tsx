7aa36f290826c8c4e820023309b55038539526e4
山﨑亮輔 <ryosuke@yamazakiryousukes-MacBook-Air.local>
2024-10-06 11:05:32 +0900

    聖遺物をインポート中の際にボタンを押せないようにした

--- src/app/artifact-scanner/components/submit-button.tsx
+++ src/app/artifact-scanner/components/submit-button.tsx
@@ -1,31 +1,35 @@
 import { artifactDataState, imageState, previewState } from "@/atoms/ocr";
 import { Button } from "@/components/ui/button";
 import { importArtifactData } from "@/lib/utils";
-import { useFormStatus } from "react-dom";
+import { useTransition } from "react";
 import { useRecoilValue, useSetRecoilState } from "recoil";
 
 const SubmitButton = () => {
-  const { pending } = useFormStatus();
   const preview = useRecoilValue(previewState);
   const image = useRecoilValue(imageState);
   const setArtifactData = useSetRecoilState(artifactDataState);
+  const [isPending, startTransition] = useTransition();
 
-  const handleImportImage = async () => {
-    if (image) {
-      const imagePath = URL.createObjectURL(image);
-      const data = (await importArtifactData(imagePath)) as ArtifactImportData;
-      setArtifactData(data);
-      console.log(data);
-    }
+  const handleImportImage = () => {
+    startTransition(async () => {
+      if (image) {
+        const imagePath = URL.createObjectURL(image);
+        const data = (await importArtifactData(
+          imagePath
+        )) as ArtifactImportData;
+        setArtifactData(data);
+        console.log(data);
+      }
+    });
   };
 
   return (
     <Button
       onClick={handleImportImage}
       className="w-full"
-      disabled={!preview || pending}
+      disabled={!preview || isPending}
     >
-      {pending ? "聖遺物をインポート中..." : "聖遺物をインポート"}
+      {isPending ? "聖遺物をインポート中..." : "聖遺物をインポート"}
     </Button>
   );
 };
