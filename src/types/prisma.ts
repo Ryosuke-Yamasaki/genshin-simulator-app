0cb77d9d636122041c31cacf18dd68febe7b5f3d
山﨑亮輔 <ryosuke@yamazakiryousukes-MacBook-Air.local>
2024-10-10 09:06:17 +0900

    props用の型定義ファイルの追加

---
+++ src/types/prisma.ts
@@ -0,0 +1,16 @@
+type artifacts = {
+  id: string;
+  set: {
+    id: string;
+    nameJp: string;
+    nameEn: string;
+    quality: number;
+  };
+  type: {
+    id: string;
+    nameJp: string;
+    nameEn: string;
+  };
+  nameJp: string;
+  nameEn: string;
+};
