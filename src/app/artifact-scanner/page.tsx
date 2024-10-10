063c28656256f0462db4de3c71542ae26c5f7a78
山﨑亮輔 <ryosuke@yamazakiryousukes-MacBook-Air.local>
2024-10-10 23:10:30 +0900

    適当に配置し直した

--- src/app/artifact-scanner/page.tsx
+++ src/app/artifact-scanner/page.tsx
@@ -2,19 +2,18 @@ import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 import ImageUploadForm from "./components/image-upload-form";
 import PreviewArtifact from "./components/preview-artifact";
-import { fetchArtifacts } from "./fetcher";
+import { fetchArtifactSets } from "./fetcher";
 import RegisterArtifactFrom from "./components/register-artifact-form";
 
 const ArtifactScannerPage = async () => {
   const session = await auth();
   if (!session) return redirect("/");
 
-  const artifacts = await fetchArtifacts();
-  console.log(artifacts);
+  const artifactSets = await fetchArtifactSets();
 
   return (
-    <div className="flex mx-10">
-      <RegisterArtifactFrom />
+    <div className="flex">
+      <RegisterArtifactFrom artifactSets={artifactSets} />
       <ImageUploadForm />
       <PreviewArtifact />
     </div>
