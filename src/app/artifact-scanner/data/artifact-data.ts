57ba2274b69dcc7a91e42ea85dd47e693186abaf
山﨑亮輔 <ryosuke@yamazakiryousukes-MacBook-Air.local>
2024-10-11 22:51:13 +0900

    パーセント表記かのプロパティの追加

--- src/app/artifact-scanner/data/artifact-data.ts
+++ src/app/artifact-scanner/data/artifact-data.ts
@@ -7,16 +7,26 @@ export const artifactTypes = [
 ];
 
 export const subStatuses = [
-  { id: "1", nameJp: "HP実数", nameEn: "HP" },
-  { id: "2", nameJp: "HP%", nameEn: "HP" },
-  { id: "3", nameJp: "攻撃力実数", nameEn: "ATK" },
-  { id: "4", nameJp: "攻撃力%", nameEn: "ATK" },
-  { id: "5", nameJp: "防御力実数", nameEn: "DEF" },
-  { id: "6", nameJp: "防御力%", nameEn: "DEF" },
-  { id: "7", nameJp: "元素熟知", nameEn: "Elemental Mastery" },
-  { id: "8", nameJp: "元素チャージ効率", nameEn: "Energy Recharge" },
-  { id: "9", nameJp: "会心率", nameEn: "CRIT Rate" },
-  { id: "10", nameJp: "会心ダメージ", nameEn: "CRIT DMG" },
+  { id: "1", nameJp: "HP実数", nameEn: "HP", isPercentage: false },
+  { id: "2", nameJp: "HP%", nameEn: "HP", isPercentage: true },
+  { id: "3", nameJp: "攻撃力実数", nameEn: "ATK", isPercentage: false },
+  { id: "4", nameJp: "攻撃力%", nameEn: "ATK", isPercentage: true },
+  { id: "5", nameJp: "防御力実数", nameEn: "DEF", isPercentage: false },
+  { id: "6", nameJp: "防御力%", nameEn: "DEF", isPercentage: true },
+  {
+    id: "7",
+    nameJp: "元素熟知",
+    nameEn: "Elemental Mastery",
+    isPercentage: false,
+  },
+  {
+    id: "8",
+    nameJp: "元素チャージ効率",
+    nameEn: "Energy Recharge",
+    isPercentage: true,
+  },
+  { id: "9", nameJp: "会心率", nameEn: "CRIT Rate", isPercentage: true },
+  { id: "10", nameJp: "会心ダメージ", nameEn: "CRIT DMG", isPercentage: true },
 ];
 
 export const mainOptions = [
