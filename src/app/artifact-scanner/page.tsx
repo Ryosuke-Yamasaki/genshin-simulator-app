1faffdcb3acfd330256cb6980b58db44f51b5f5d
山﨑亮輔 <ryosuke@yamazakiryousukes-MacBook-Air.local>
2024-10-12 20:40:06 +0900

    関数名の変更

--- src/app/artifact-scanner/page.tsx
+++ src/app/artifact-scanner/page.tsx
@@ -2,14 +2,14 @@ import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 import ImageUploadForm from "./components/image-upload-form";
 import PreviewArtifact from "./components/preview-artifact";
-import { fetchArtifactSets } from "./fetcher";
+import { getArtifactSets } from "./lib/fetcher";
 import RegisterArtifactForm from "./components/register-artifact-form";
 
 const ArtifactScannerPage = async () => {
   const session = await auth();
   if (!session) return redirect("/");
 
-  const artifactSets = await fetchArtifactSets();
+  const artifactSets = await getArtifactSets();
 
   return (
     <div className="flex">
