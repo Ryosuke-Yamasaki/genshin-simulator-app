d3ee6da6b8756663f13c516f140161308b009f29
山﨑亮輔 <ryosuke@yamazakiryousukes-MacBook-Air.local>
2024-10-13 21:21:38 +0900

    zodschemaの変更

--- src/app/artifact-scanner/schema.ts
+++ src/app/artifact-scanner/schema.ts
@@ -1,16 +1,19 @@
 import { z } from "zod";
 
+export const postArtifacterSubOptionSchema = z.object({
+  attribute: z.string({ required_error: "サブステータスは必須です" }),
+  value: z.string({ required_error: "サブステータスは必須です" }),
+});
+
 export const postArtifacterSchema = z.object({
   setId: z.string({ required_error: "セット効果は必須です" }),
   quality: z.string({ required_error: "セット効果は必須です" }),
   typeId: z.string({ required_error: "装備部位は必須です" }),
   mainOption: z.string({ required_error: "メインオプションは必須です" }),
-  subOptionAttribute0: z.string({ required_error: "サブオプションは必須です" }),
-  subOptionValue0: z.string({ required_error: "サブオプションは必須です" }),
-  subOptionAttribute1: z.string({ required_error: "サブオプションは必須です" }),
-  subOptionValue1: z.string({ required_error: "サブオプションは必須です" }),
-  subOptionAttribute2: z.string({ required_error: "サブオプションは必須です" }),
-  subOptionValue2: z.string({ required_error: "サブオプションは必須です" }),
-  subOptionAttribute3: z.string({ required_error: "サブオプションは必須です" }),
-  subOptionValue3: z.string({ required_error: "サブオプションは必須です" }),
+  subOptions: z.array(
+    z.object({
+      attribute: z.string({ required_error: "サブステータスは必須です" }),
+      value: z.string({ required_error: "サブステータスは必須です" }),
+    })
+  ),
 });
