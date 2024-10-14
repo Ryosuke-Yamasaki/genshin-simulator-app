b3ed528415e4d69bc48ed64939689b9ec7c5f13d
山﨑亮輔 <ryosuke@yamazakiryousukes-MacBook-Air.local>
2024-10-14 21:55:01 +0900

    聖遺物のインポートをServerActionsで処理するように変更

--- src/app/artifact-scanner/components/image-upload-form.tsx
+++ src/app/artifact-scanner/components/image-upload-form.tsx
@@ -2,12 +2,14 @@
 
 import { CardFooter } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import SubmitButton from "./submit-button";
 import { useRecoilState, useSetRecoilState } from "recoil";
 import { imageState, previewState } from "@/app/artifact-scanner/state";
 import Image from "next/image";
 import FormWrapper from "./form-wrapper";
 import FormLabel from "./ui/form-label";
+import { useFormState } from "react-dom";
+import { importArtifact } from "../actions";
+import { Button } from "@/components/ui/button";
 
 const ImageUploadForm = () => {
   const [preview, setPreview] = useRecoilState(previewState);
@@ -28,35 +30,41 @@ const ImageUploadForm = () => {
     }
   };
 
+  const [message, formAction] = useFormState(importArtifact, null);
+
   return (
     <FormWrapper formTitle="聖遺物のインポート">
-      <div className="space-y-4">
-        <div>
-          <FormLabel htmlFor="file" labelName="画像の選択" />
-          <Input
-            id="file"
-            name="file"
-            type="file"
-            accept="image/*"
-            onChange={handleChange}
-          />
-        </div>
-        {preview && (
-          <div className="mt-4">
-            <FormLabel labelName="プレビュー" />
-            <Image
-              src={preview}
-              alt="プレビュー"
-              width={500}
-              height={500}
-              className="max-w-full h-auto mt-1 rounded-lg border border-gray-200"
+      <form action={formAction}>
+        <div className="space-y-4">
+          <div>
+            <FormLabel htmlFor="file" labelName="画像の選択" />
+            <Input
+              id="file"
+              name="file"
+              type="file"
+              accept="image/*"
+              onChange={handleChange}
             />
           </div>
-        )}
-      </div>
-      <CardFooter className="px-0 pt-6">
-        <SubmitButton />
-      </CardFooter>
+          {preview && (
+            <div className="mt-4">
+              <FormLabel labelName="プレビュー" />
+              <Image
+                src={preview}
+                alt="プレビュー"
+                width={500}
+                height={500}
+                className="max-w-full h-auto mt-1 rounded-lg border border-gray-200"
+              />
+            </div>
+          )}
+        </div>
+        <CardFooter className="px-0 pt-6">
+          <Button type="submit" className="w-full">
+            聖遺物をインポート
+          </Button>
+        </CardFooter>
+      </form>
     </FormWrapper>
   );
 };
