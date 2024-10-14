158ebb790e318a348f4f665dc617e240a6dcc680
山﨑亮輔 <ryosuke@yamazakiryousukes-MacBook-Air.local>
2024-10-14 21:48:05 +0900

    必要ない型を削除

--- src/types/prisma.ts
+++ src/types/prisma.ts
@@ -1,24 +1,3 @@
-type artifact = {
-  id: string;
-  set: artifactSet;
-  type: artifactType;
-  nameJp: string;
-  nameEn: string;
-};
-
-type artifactSet = {
-  id: string;
-  nameJp: string;
-  nameEn: string;
-  quality: string;
-};
-
-type artifactType = {
-  id: string;
-  nameJp: string;
-  nameEn: string;
-};
-
 type artifacter = {
   userId: string;
   artifactId: string;
@@ -28,5 +7,5 @@ type artifacter = {
 type subOption = {
   artifacterId: string;
   subStatusId: string;
-  value: number;
+  value: string;
 };
