c9490fd8d9e6051b8a982b37f11c93cca181571d
山﨑亮輔 <ryosuke@yamazakiryousukes-MacBook-Air.local>
2024-10-18 22:58:33 +0900

    data-table.tsxのディレクトリを変更

--- src/app/artifacter/components/data-table.tsx
+++ src/app/artifacter/components/data-table.tsx
@@ -2,8 +2,10 @@
 
 import {
   ColumnDef,
+  ColumnFiltersState,
   flexRender,
   getCoreRowModel,
+  getFilteredRowModel,
   getPaginationRowModel,
   getSortedRowModel,
   SortingState,
@@ -18,8 +20,11 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import DataTablePagination from "./data-table-pagination";
+import DataTablePagination from "./ui/data-table-pagination";
 import { useState } from "react";
+import { DataTableFilterSelector } from "./ui/data-table-filter-selector";
+import { Button } from "@/components/ui/button";
+import { Artifacter } from "../types/prisma";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -33,6 +38,7 @@ export function DataTable<TData, TValue>({
   const [sorting, setSorting] = useState<SortingState>([
     { id: "score", desc: true },
   ]);
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
   const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
 
   const table = useReactTable({
@@ -42,15 +48,85 @@ export function DataTable<TData, TValue>({
     getPaginationRowModel: getPaginationRowModel(),
     onSortingChange: setSorting,
     getSortedRowModel: getSortedRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
     state: {
       sorting,
+      columnFilters,
       columnVisibility,
     },
-    onColumnVisibilityChange: setColumnVisibility,
+  });
+
+  const artifacters = data as Artifacter[];
+  const artifactSets = Array.from(
+    new Map(
+      artifacters.map((artifacter) => [
+        artifacter.artifact.setId,
+        artifacter.artifact.set,
+      ])
+    ).values()
+  );
+  const artifactSetsOption = artifactSets.map(({ nameJp }) => {
+    return { value: nameJp, label: nameJp };
+  });
+  const artifactTypes = Array.from(
+    new Map(
+      artifacters.map((artifacter) => [
+        artifacter.artifact.typeId,
+        artifacter.artifact.type,
+      ])
+    ).values()
+  );
+  const artifactTypesOption = artifactTypes.map(({ nameJp }) => {
+    return { value: nameJp, label: nameJp };
+  });
+  const artifactMainStatuses = Array.from(
+    new Map(
+      artifacters.map((artifacter) => [
+        artifacter.mainOptionId,
+        artifacter.mainOption,
+      ])
+    ).values()
+  );
+  const artifactMainStatusesOption = artifactMainStatuses.map(({ nameJp }) => {
+    return { value: nameJp, label: nameJp };
   });
 
   return (
     <div className="space-y-2">
+      <div className="flex flex-1 items-center space-x-2">
+        {table.getColumn("artifactSet") && (
+          <DataTableFilterSelector
+            column={table.getColumn("artifactSet")}
+            title="セット効果"
+            options={artifactSetsOption}
+          />
+        )}
+        {table.getColumn("artifactType") && (
+          <DataTableFilterSelector
+            column={table.getColumn("artifactType")}
+            title="部位"
+            options={artifactTypesOption}
+          />
+        )}
+        {table.getColumn("mainOption") && (
+          <DataTableFilterSelector
+            column={table.getColumn("mainOption")}
+            title="メインオプション"
+            options={artifactMainStatusesOption}
+          />
+        )}
+        {table.getState().columnFilters.length > 0 && (
+          <Button
+            variant="ghost"
+            onClick={() => table.resetColumnFilters()}
+            className="h-8 px-2 lg:px-3"
+          >
+            リセット
+          </Button>
+        )}
+      </div>
       <div className="rounded-md border">
         <Table>
           <TableHeader>
