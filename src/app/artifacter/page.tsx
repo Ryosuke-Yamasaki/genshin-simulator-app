3cb51e065594c2c8edf52e62f28580f93a6ba1b5
山﨑亮輔 <ryosuke@yamazakiryousukes-MacBook-Air.local>
2024-10-18 22:58:51 +0900

    フィルター機能を追加

--- src/app/artifacter/page.tsx
+++ src/app/artifacter/page.tsx
@@ -1,18 +1,18 @@
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 import { getArtifactersByUser } from "./lib/prisma";
-import { DataTable } from "./components/ui/data-table";
+import { DataTable } from "./components/data-table";
 import { columns } from "./components/columns";
 
 const ArtifacterPage = async () => {
   const session = await auth();
   if (!session || !session.user?.id) return redirect("/");
 
-  const artifacter = await getArtifactersByUser(session.user.id);
+  const artifacters = await getArtifactersByUser(session.user.id);
 
   return (
     <div className="mx-6 py-10">
-      <DataTable columns={columns} data={artifacter} />
+      <DataTable columns={columns} data={artifacters} />
     </div>
   );
 };
